c4e98f704c2c2ed2fe928c37eb72868dfece9a9f
Mircea Teodor Oprea <mirceateodor.oprea@gmail.com>
2022-01-17 21:26:30 +0200

    Add basic function as content for all the lambdas for testing

--- rust-crud/lambda-functions/create-user/src/main.rs
+++ rust-crud/lambda-functions/create-user/src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use lambda_runtime::{Context, Error};
+use serde::{Serialize, Deserialize};
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    let handler = lambda_runtime::handler_fn(handler);
+    lambda_runtime::run(handler).await?;
+    Ok(())
+}
+
+#[derive(Deserialize)]
+struct Event {
+    username: String,
+}
+
+#[derive(Serialize)]
+struct Output {
+    message: String,
+}
+
+async fn handler(event: Event, context: Context) -> Result<Output, Error> {
+    let message = format!("Welcome, {}!", event.username);
+    Ok(Output {
+        message: message,
+    })
 }
