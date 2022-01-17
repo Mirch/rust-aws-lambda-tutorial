128e1ee463f4079ffd0286c27b192d2d427c85c4
Mircea Teodor Oprea <mirceateodor.oprea@gmail.com>
2022-01-17 23:22:34 +0200

    Add basic lambda and API Gateway IaC code

--- rust-crud/lib/rust-crud-stack.js
+++ rust-crud/lib/rust-crud-stack.js
@@ -1,5 +1,5 @@
 const { Stack, Duration } = require('aws-cdk-lib');
-// const sqs = require('aws-cdk-lib/aws-sqs');
+const lambda = require("aws-cdk-lib/aws-lambda");
 
 class RustCrudStack extends Stack {
   /**
@@ -11,12 +11,58 @@ class RustCrudStack extends Stack {
   constructor(scope, id, props) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const getUserHandler = new lambda.Function(this, "GetUserHandler", {
+      runtime: lambda.Runtime.PROVIDED_AL2, 
+      code: lambda.Code.fromAsset("../lambda-functions/get-user/lambda.zip"),
+      handler: "handler"
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'RustCrudQueue', {
-    //   visibilityTimeout: Duration.seconds(300)
-    // });
+    const createUserHandler = new lambda.Function(this, "CreateUserHandler", {
+      runtime: lambda.Runtime.PROVIDED_AL2, 
+      code: lambda.Code.fromAsset("../lambda-functions/create-user/lambda.zip"),
+      handler: "handler"
+    });
+
+    const updateUserHandler = new lambda.Function(this, "UpdateUserHandler", {
+      runtime: lambda.Runtime.PROVIDED_AL2, 
+      code: lambda.Code.fromAsset("../lambda-functions/update-user/lambda.zip"),
+      handler: "handler"
+    });
+
+    const deleteUserHandler = new lambda.Function(this, "DeleteUserHandler", {
+      runtime: lambda.Runtime.PROVIDED_AL2, 
+      code: lambda.Code.fromAsset("../lambda-functions/delete-user/lambda.zip"),
+      handler: "handler"
+    });
+
+    const api = new apigateway.RestApi(this, "users-api", {
+      restApiName: "Users Service",
+      description: "This service serves users."
+    });
+
+    const getUserIntegration = new apigateway.LambdaIntegration(getUserHandler, {
+      requestTemplates: { "application/json": '{ "statusCode": "200" }' }
+    });
+
+    api.root.addMethod("GET", getUserIntegration);
+
+    const createUserIntegration = new apigateway.LambdaIntegration(createUserHandler, {
+      requestTemplates: { "application/json": '{ "statusCode": "200" }' }
+    });
+
+    api.root.addMethod("POST", createUserIntegration);
+
+    const updateUserIntegration = new apigateway.LambdaIntegration(updateUserHandler, {
+      requestTemplates: { "application/json": '{ "statusCode": "200" }' }
+    });
+
+    api.root.addMethod("PUT", updateUserIntegration);
+
+    const deleteUserIntegration = new apigateway.LambdaIntegration(deleteUserHandler, {
+      requestTemplates: { "application/json": '{ "statusCode": "200" }' }
+    });
+
+    api.root.addMethod("DELETE", deleteUserIntegration);
   }
 }
 
