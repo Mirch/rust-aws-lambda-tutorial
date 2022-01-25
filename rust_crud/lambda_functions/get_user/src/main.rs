88f5ddd6602f28009bf02034252a1226e69c8deb
Mircea Teodor Oprea <mirceateodor.oprea@gmail.com>
2022-01-25 22:18:22 +0200

    Remove logs

--- rust_crud/lambda_functions/get_user/src/main.rs
+++ rust_crud/lambda_functions/get_user/src/main.rs
@@ -6,13 +6,11 @@ use lambda_http::{
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    println!("Hello!");
     lambda_runtime::run(handler(func)).await?;
     Ok(())
 }
 
 async fn func(event: Request, _: Context) -> Result<impl IntoResponse, Error> {
-    println!("{:?}", event);
     Ok(match event.query_string_parameters().get("username") {
         Some(username) => format!("Hello, {}!", username).into_response(),
         _ => Response::builder()
