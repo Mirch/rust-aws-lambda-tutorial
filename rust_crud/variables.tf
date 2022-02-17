6ab5bf4031180111e186bba7f6fc58844c1d1e74
Mircea Teodor Oprea <mirceateodor.oprea@gmail.com>
2022-02-17 23:29:53 +0200

    Fix variables

--- rust_crud/variables.tf
+++ rust_crud/variables.tf
@@ -11,26 +11,26 @@ variable "get_user_bin_path" {
   description = "The binary path for the get_user lambda."
 
   type    = string
-  default = ".bin/get_user/bootstrap"
+  default = "./bin/get_user/bootstrap"
 }
 
 variable "create_user_bin_path" {
   description = "The binary path for the create_user lambda."
 
   type    = string
-  default = ".bin/create_user/bootstrap"
+  default = "./bin/create_user/bootstrap"
 }
 
 variable "update_user_bin_path" {
   description = "The binary path for the update_user lambda."
 
   type    = string
-  default = ".bin/update_user/bootstrap"
+  default = "./bin/update_user/bootstrap"
 }
 
 variable "delete_user_bin_path" {
   description = "The binary path for the delete_user lambda."
 
   type    = string
-  default = ".bin/delete_user/bootstrap"
+  default = "./bin/delete_user/bootstrap"
 }
