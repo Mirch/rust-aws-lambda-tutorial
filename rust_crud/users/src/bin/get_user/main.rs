240c70ab6d83694b12b4ef1fe6547586bf2283b9
Mircea Teodor Oprea <mirceateodor.oprea@gmail.com>
2022-02-17 21:25:05 +0200

    Replace GetUser with old code

--- rust_crud/users/src/bin/get_user/main.rs
+++ rust_crud/users/src/bin/get_user/main.rs
@@ -1,15 +1,8 @@
-
-use lambda_layer::environment::{get_env_variable};
-
 use lambda_http::{
     handler,
     lambda_runtime::{self, Context, Error},
     IntoResponse, Request, RequestExt, Response,
 };
-use aws_sdk_dynamodb::{Client};
-use aws_sdk_dynamodb::model::AttributeValue;
-
-static USERS_TABLE_NAME: &str = "USERS";
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
@@ -18,27 +11,11 @@ async fn main() -> Result<(), Error> {
 }
 
 async fn func(event: Request, _: Context) -> Result<impl IntoResponse, Error> {
-
-    let query_string = event.query_string_parameters();
-
-    let shared_config = aws_config::load_from_env().await;
-    let client = Client::new(&shared_config);
-    
-    let table_name = get_env_variable(USERS_TABLE_NAME);
-    let username = match query_string.get("username") {
-        Some(value) => String::from(value),
-        _ => panic!("No username provided!")
-    };
-    let req = client
-        .get_item()
-        .table_name(table_name)
-        .key("username", AttributeValue::S(username));
-    let response = match req.send().await {
-        Ok(r) => r,
-        Err(e) => panic!("Could not find the user.")
-    };
-
-    return Ok(Response::builder()
-        .status(200)
-        .body(response.item));
+    Ok(match event.query_string_parameters().get("username") {
+        Some(username) => format!("Hello, {}!", username).into_response(),
+        _ => Response::builder()
+            .status(400)
+            .body("Empty first name".into())
+            .expect("failed to render response"),
+    })
 }
